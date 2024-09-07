1776d97cfa918ba0b7ff1790551e0a15066b1694
Seunoluwa1991 <kilashomichael@gmail.com>
2024-09-07 10:54:33 +0100

    17thcommit

--- CSSFILE/script.js
+++ CSSFILE/script.js
@@ -1,3 +1,11 @@
-#click{
-    document.getElementById("click").style.fontSize = "500px";
+function myFunction(){
+    document.getElementById("click").style.fontSize= "100px"
 }
+
+
+let e = 7;
+let f = 8;
+let z = ( e + f) * 8;
+
+console.log(z);
+
